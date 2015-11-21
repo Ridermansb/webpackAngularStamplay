be9cc943edd72b9359def275e6dc5853166ea3b8
ridermansb <ridermansb@gmail.com>
2015-11-21 19:36:17 -0200

    Implements app,js with logon support

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
-console.log('Configuring stamplay.. ');
-
 Stamplay.init('webpackangularstamplay');
+
+var angular = require('angular');
+
+var AuthService = require('./www/components/shared/AuthService');
+var LoginController = require('./www/components/home/loginController');
+
+function registerStates($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/');
+  $stateProvider
+      .state('anon', {
+          abstract: true,
+          template: "<ui-view/>"
+      })
+      .state('anon.login', {
+          url: '/',
+          controller: LoginController,
+          controllerAs: 'vm',
+          template: require('html!./www/components/home/loginView.html')
+      })
+}
+
+var uiRouter = require('angular-ui-router');
+
+module.exports =
+    angular.module('app', [uiRouter])
+    .config(['$stateProvider', '$urlRouterProvider', registerStates])
+    .factory('Auth', AuthService)
+    .controller('LoginController', LoginController)
+    ;
